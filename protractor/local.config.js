611499b4aebc46dfdccb46d7823a765c3cc93fc8
Felipe Castillo <jfcastillo1204@gmail.com>
2020-07-30 10:04:10 -0500

    step 3

--- protractor/local.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.config = void 0;
-exports.config = {
-  framework: "jasmine",
-  specs: ["../test/google.spec.js"],
-  seleniumAddress: "http://localhost:4444/wd/hub",
-};
