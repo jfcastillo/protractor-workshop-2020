804df54209f8b49323870780131f8afd2912605d
Felipe Castillo <jfcastillo1204@gmail.com>
2020-07-29 22:30:27 -0500

    files removed

--- protractor/local.config.ts
+++
@@ -1,7 +0,0 @@
-import { Config } from "protractor";
-
-export const config: Config = {
-  framework: "jasmine",
-  specs: ["../test/google.spec.js"],
-  seleniumAddress: "http://localhost:4444/wd/hub",
-};
